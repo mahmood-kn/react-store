7a41b26d8606495ae95f787b756bf7b51f7e796a
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-06-07 23:48:52 +0430

    starting with navbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '@fontsource/roboto';
+import 'typeface-poppins';
+
+import {
+  unstable_createMuiStrictModeTheme as createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Poppins', 'serif'].join(','),
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../../components/Navbar';
+
+interface Props {}
+
+const Home = (props: Props) => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
