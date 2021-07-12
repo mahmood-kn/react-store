067e1bed4c2f07427e6fc37b11fa3e07dc6008cf
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-07-12 23:30:24 +0430

    product skeleton

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -6,10 +6,37 @@ import ProductDetail from './ProductDetail';
 import Container from '@material-ui/core/Container';
 import { getSingleProduct } from 'store/mainAsync';
 import MoreDetails from './MoreDetails';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    skeleton: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      [theme.breakpoints.down('md')]: {
+        flexDirection: 'column',
+      },
+    },
+    skeletonImg: {
+      width: '100%',
+    },
+    skeletonLineContainer: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    skeletonLine: {
+      margin: '0.5rem 0',
+    },
+  })
+);
 
 interface Props {}
 
 const Product = (props: Props) => {
+  const classes = useStyles();
   const dispatch = useAppDispatch();
   const params = useParams<{ id: string }>();
   useEffect(() => {
@@ -31,7 +58,39 @@ const Product = (props: Props) => {
         ) : product === null && !loading ? (
           'Not Found'
         ) : (
-          'Loading...'
+          <div className={classes.skeleton}>
+            <Skeleton
+              animation='wave'
+              className={classes.skeletonImg}
+              height='500px'
+            />
+            <div className={classes.skeletonLineContainer}>
+              <Skeleton
+                animation='wave'
+                className={classes.skeletonLine}
+                width='40%'
+                height='20px'
+              />
+              <Skeleton
+                animation='wave'
+                className={classes.skeletonLine}
+                width='40%'
+                height='20px'
+              />
+              <Skeleton
+                animation='wave'
+                className={classes.skeletonLine}
+                width='40%'
+                height='20px'
+              />
+              <Skeleton
+                animation='wave'
+                className={classes.skeletonLine}
+                width='40%'
+                height='20px'
+              />
+            </div>
+          </div>
         )}
       </Container>
     </>
